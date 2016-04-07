96183ed41183011eb686927c990589bf7307047a
Nate Barbettini <nathanaelb@gmail.com>
2016-04-06 18:06:11 -0700

    Add goodies to /manage

---
+++ src/StormpathExample/Controllers/ManageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StormpathExample.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
