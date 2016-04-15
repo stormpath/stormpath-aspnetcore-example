06f45615f3de36aeab97037b5f16cb16778df9e8
Nate Barbettini <nathanaelb@gmail.com>
2016-04-15 13:26:58 -0700

    Add a Web API example

---
+++ src/StormpathExample/Controllers/ProtectedValuesController.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="ProtectedValuesController.cs" company="Stormpath, Inc.">
+// Copyright (c) 2016 Stormpath, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace StormpathExample.Controllers
+{
+    // This API controller is protected and requires either an active Stormpath cookie
+    // or Bearer authentication.
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProtectedValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "hello", "secure", "world" };
+        }
+    }
+}
